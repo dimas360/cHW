9634baede5f1a2ad4deb4f8c347d4af5e702e9ab
dimas360 <bios5524@yandex.ru>
2022-11-22 16:14:49 +0300

    lq

---
+++ e3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день
+// недели, и проверяет, является ли этот день выходным.
+
+using static System.Console;
+Clear();
+
+bool isHoliday(int day) { return day>5 ? true : false; }
+int day;
+do
+{
+    Write("введите число дня недели : ");
+    day = int.Parse(ReadLine());
+} while (day > 7||day<1);
+
+Write($"этот день выходной? ");
+if (isHoliday(day)) Write("да");
+else Write("нет.");
